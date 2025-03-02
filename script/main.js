3c714aec11323c2d7a0f7bacda600c77402a9e09
kuailexiaomai <121001149+kuailexiaomai@users.noreply.github.com>
2025-03-02 17:16:07 +0800

    Update main.js

--- script/main.js
+++ script/main.js
@@ -221,8 +221,8 @@ const animationTimeline = () => {
       "-=2"
     )
     .from(".hat", 0.5, {
-      x: -50,
-      y: 300,
+      x: 0,
+      y: 500,
       rotation: -180,
       opacity: 0
     })
